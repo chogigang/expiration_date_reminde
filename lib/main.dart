cfb8543f5ec6454b57e04496d1f4a22373f9c7f0
chogigang <cbh2031@gmail.com>
2024-06-08 20:24:28 +0900

    파이어 베이스 알람 진행중

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'dart:convert';
 import 'package:expiration_date/data/database.dart';
 import 'package:expiration_date/homepage.dart';
+import 'package:expiration_date/module/notification.dart';
+import 'package:expiration_date/module/push_notification_initializer.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk_talk.dart' as kakao;
 import 'module/firebase_options.dart';
 import 'package:get_it/get_it.dart';
-import 'module/notification.dart'; // FlutterLocalNotifications 가져오기
+
+final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
   // 드리프트 초기화
@@ -16,13 +21,17 @@ void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Kakao SDK 초기화
+  kakao.KakaoSdk.init(nativeAppKey: 'f97d27c81aa369408064bb7c68f9fd1e');
+
   // Firebase 초기화
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  // Kakao SDK 초기화
-  kakao.KakaoSdk.init(nativeAppKey: 'f97d27c81aa369408064bb7c68f9fd1e');
+  // 알림 초기화
+  final pushNotificationInitializer = PushNotificationInitializer(navigatorKey);
+  await pushNotificationInitializer.init();
 
   runApp(const MyApp());
 }
@@ -48,8 +57,9 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      navigatorKey: navigatorKey,
+      home: const HomePage(),
     );
   }
 }
---
+++ lib/module/const/app.dart
@@ -0,0 +1,8 @@
+// TODO Implement this library.
+
+import 'package:flutter/material.dart';
+
+class App {
+  static final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey<NavigatorState>();
+}
