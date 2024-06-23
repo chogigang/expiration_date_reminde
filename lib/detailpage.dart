b572ff3b54eb1e67a75376df29e62551aec6a1f0
chogigang <cbh2031@gmail.com>
2024-06-23 16:25:07 +0900

    코드 설명 ,로그인 오류 발생

--- lib/detailpage.dart
+++ lib/detailpage.dart
@@ -1,142 +1,161 @@
-import 'dart:convert';
-import 'dart:typed_data';
+import 'dart:convert'; // JSON 인코딩 및 디코딩을 위한 라이브러리
+import 'dart:typed_data'; // 바이트 데이터 처리를 위한 라이브러리
 
-import 'package:flutter/material.dart';
-import 'package:expiration_date/data/database.dart';
-import 'package:http/http.dart' as http;
-import 'package:html/parser.dart' as parser;
-import 'package:expiration_date/module/local_push_notification.dart';
-import 'package:expiration_date/module/notification.dart';
-import 'package:expiration_date/recipepage.dart';
+import 'package:flutter/material.dart'; // Flutter의 주요 UI 라이브러리
+import 'package:expiration_date/data/database.dart'; // 데이터베이스 관련 클래스 임포트
+import 'package:http/http.dart' as http; // HTTP 요청을 위한 라이브러리
+import 'package:html/parser.dart' as parser; // HTML 파싱을 위한 라이브러리
+import 'package:expiration_date/module/local_push_notification.dart'; // 로컬 푸시 알림 모듈 임포트
+import 'package:expiration_date/module/notification.dart'; // 알림 관련 모듈 임포트
+import 'package:expiration_date/recipepage.dart'; // 레시피 페이지 관련 클래스 임포트
 
+// DetailPage 클래스 정의, StatefulWidget을 상속받음
 class DetailPage extends StatefulWidget {
-  final FooddbData foodItem;
+  final FooddbData foodItem; // foodItem을 DetailPage 생성자에서 받음
 
   const DetailPage({Key? key, required this.foodItem}) : super(key: key);
 
   @override
-  _DetailPageState createState() => _DetailPageState();
+  _DetailPageState createState() =>
+      _DetailPageState(); // _DetailPageState 클래스 생성
 }
 
+// _DetailPageState 클래스 정의
 class _DetailPageState extends State<DetailPage> {
   @override
   void initState() {
-    listenNotifications();
-    super.initState();
+    listenNotifications(); // 알림 리스너 설정
+    super.initState(); // 부모 클래스의 initState 호출
   }
 
+  // 알림 수신 리스너 설정
   void listenNotifications() {
     LocalPushNotifications.notificationStream.stream.listen((String? payload) {
       if (payload != null) {
-        print('Received payload: $payload');
-        Navigator.pushNamed(context, '/message', arguments: payload);
+        print('Received payload: $payload'); // 수신된 페이로드 출력
+        Navigator.pushNamed(context, '/message',
+            arguments: payload); // 특정 경로로 네비게이트
       }
     });
   }
 
+  // 레시피 정보를 가져와서 화면에 표시하는 메서드
   Future<void> _showRecipes(BuildContext context, String searchQuery) async {
-    final apiKey = '856346fdbba3479482aa';
+    final apiKey = '856346fdbba3479482aa'; // API 키
     final url = Uri.parse(
-        'http://openapi.foodsafetykorea.go.kr/api/$apiKey/COOKRCP01/json/1/5/RCP_NM=$searchQuery');
-    try {
-      final response = await http.get(url);
+        'http://openapi.foodsafetykorea.go.kr/api/$apiKey/COOKRCP01/json/1/5/RCP_NM=$searchQuery'); // 레시피 검색 URL
 
-      print('Response status: ${response.statusCode}');
-      print('Response body: ${response.body}');
+    try {
+      final response = await http.get(url); // HTTP GET 요청
+
+      print('Response status: ${response.statusCode}'); // 응답 상태 코드 출력
+      print('Response body: ${response.body}'); // 응답 본문 출력
 
       if (response.statusCode == 200) {
-        final jsonResponse = jsonDecode(response.body);
-        print('JSON Response: $jsonResponse');
+        final jsonResponse = jsonDecode(response.body); // JSON 디코딩
+        print('JSON Response: $jsonResponse'); // 디코딩된 JSON 출력
 
-        final recipes = jsonResponse['COOKRCP01']?['row'];
+        final recipes = jsonResponse['COOKRCP01']?['row']; // 레시피 데이터 추출
 
         if (recipes != null && recipes is List) {
           Navigator.push(
             context,
             MaterialPageRoute(
               builder: (context) => RecipePage(
-                  initialRecipes: recipes, initialSearchQuery: searchQuery),
+                  initialRecipes: recipes,
+                  initialSearchQuery: searchQuery), // 레시피 페이지로 이동
             ),
           );
         } else {
-          print('No recipes found');
+          print('해당 레시피가 없습니다.'); // 레시피가 없을 경우 출력
           ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('No recipes found')),
+            const SnackBar(content: Text('해당 레시피가 없습니다.')), // 스낵바 표시
           );
         }
       } else {
-        print('Failed to load recipes');
+        print('요청 실패'); // 요청 실패 시 출력
         ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Failed to load recipes')),
+          const SnackBar(content: Text('레시피 요청 실패')), // 스낵바 표시
         );
       }
     } catch (e) {
-      print('Error occurred: $e');
+      print('Error occurred: $e'); // 예외 발생 시 출력
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
-            content: Text('An error occurred while fetching recipes')),
+            content:
+                Text('An error occurred while fetching recipes')), // 스낵바 표시
       );
     }
   }
 
+  // 위젯 빌드 메서드
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.foodItem.name),
+        title: Text(widget.foodItem.name), // 앱바에 음식 이름 표시
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            // 알림 버튼
             TextButton(
               onPressed: () => FlutterLocalNotification.showNotification(),
-              child: const Text("Send Notification"),
+              child: const Text("알람버튼"),
             ),
+            // 알람 버튼 2
             ElevatedButton(
               onPressed: () {
                 LocalPushNotifications.showSimpleNotification(
-                    title: "Regular Notification Title",
-                    body: "Regular Notification Body",
+                    title: "유통기한 알람",
+                    body: "유통기한 임박!",
                     payload: "Regular Notification Data");
               },
-              child: const Text('Regular Notification'),
+              child: const Text('알람 버튼 2'),
             ),
+            // 이미지가 있으면 표시
             if (widget.foodItem.image_data != null)
               Image.memory(widget.foodItem.image_data!),
             if (widget.foodItem.image_url != null &&
                 widget.foodItem.image_url!.isNotEmpty)
               Image.network(widget.foodItem.image_url!),
             const SizedBox(height: 10),
+            // 음식 이름 표시
             Text(
-              'Food Name: ${widget.foodItem.name}',
+              '이름 : ${widget.foodItem.name}',
               style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 10),
+            // 유통기한 표시
             Text(
-              'Expiry Date: ${widget.foodItem.expiry_date.toIso8601String().split('T')[0]}',
+              '날짜 : ${widget.foodItem.expiry_date.toIso8601String().split('T')[0]}',
               style: const TextStyle(fontSize: 18),
             ),
             const SizedBox(height: 10),
+            // 음식 종류 표시
             Text(
-              'Type: ${widget.foodItem.type}',
+              '종류 : ${widget.foodItem.type}',
               style: const TextStyle(fontSize: 18),
             ),
             const SizedBox(height: 10),
+            // 알람 주기 표시
             Text(
-              'Alarm Cycle: ${widget.foodItem.alarm_cycle ?? 'Not set'}',
+              '알람 주기 : ${widget.foodItem.alarm_cycle ?? 'Not set'}',
               style: const TextStyle(fontSize: 18),
             ),
             const SizedBox(height: 10),
+            // 생성 날짜 표시
             Text(
-              'Registration Date: ${widget.foodItem.createdAt.toIso8601String().split('T')[0]}',
+              '유통기한 날짜 : ${widget.foodItem.createdAt.toIso8601String().split('T')[0]}',
               style: const TextStyle(fontSize: 18),
             ),
             const SizedBox(height: 20),
+            // 레시피 보기 버튼
             ElevatedButton(
               onPressed: () => _showRecipes(context, widget.foodItem.name),
-              child: const Text('View Recipes'),
+              child: const Text('레시피 보기'),
             ),
           ],
         ),
--- lib/module/cameraservice.dart
+++ lib/module/cameraservice.dart
@@ -1,62 +1,70 @@
-import 'dart:typed_data';
-import 'dart:convert';
-import 'package:camera/camera.dart';
-import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:html/parser.dart' as parser;
+import 'dart:typed_data'; // 바이너리 데이터 처리를 위해 사용
+import 'dart:convert'; // JSON 데이터 처리를 위해 사용
+import 'package:camera/camera.dart'; // 카메라 기능을 사용하기 위해 필요한 패키지
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart'; // 텍스트 인식 기능을 사용하기 위한 패키지
+import 'package:permission_handler/permission_handler.dart'; // 권한 요청을 처리하기 위한 패키지
+import 'package:flutter/material.dart'; // Flutter의 기본 UI 구성 요소
+import 'package:http/http.dart' as http; // HTTP 요청을 보내기 위한 패키지
+import 'package:html/parser.dart' as parser; // HTML 파싱을 위한 패키지
 
 class CameraService {
-  CameraController? _cameraController;
-  String imageUrl = ''; // 이미지 url
+  CameraController? _cameraController; // 카메라 컨트롤러 인스턴스
+  String imageUrl = ''; // 이미지 URL을 저장할 변수
 
+  // 카메라 초기화 메서드
   Future<void> initCamera() async {
-    final cameras = await availableCameras();
+    final cameras = await availableCameras(); // 사용 가능한 카메라 목록 가져오기
     if (cameras.isNotEmpty) {
       _cameraController = CameraController(
-        cameras[0],
-        ResolutionPreset.medium,
-        enableAudio: false,
+        cameras[0], // 첫 번째 카메라 선택
+        ResolutionPreset.medium, // 해상도 설정
+        enableAudio: false, // 오디오 비활성화
       );
-      await _cameraController!.initialize();
-      await _cameraController!.setFlashMode(FlashMode.off);
+      await _cameraController!.initialize(); // 카메라 초기화
+      await _cameraController!.setFlashMode(FlashMode.off); // 플래시 비활성화
     }
   }
 
+  // 카메라 컨트롤러를 반환하는 getter
   CameraController? get cameraController => _cameraController;
 
+  // 특정 권한을 요청하는 메서드
   Future<void> handlePermission(Permission permission) async {
-    await permission.request();
+    await permission.request(); // 권한 요청
   }
 
+  // 사진을 촬영하고 텍스트 인식을 수행하는 메서드
   Future<void> takePicture(Function(Uint8List?, String?) callback) async {
     if (_cameraController != null && _cameraController!.value.isInitialized) {
       try {
-        final image = await _cameraController!.takePicture();
-        final imagePath = image.path;
-        final imageData = await image.readAsBytes();
+        final image = await _cameraController!.takePicture(); // 사진 촬영
+        final imagePath = image.path; // 사진 경로
+        final imageData = await image.readAsBytes(); // 이미지 데이터를 바이트 배열로 읽기
 
-        final inputImage = InputImage.fromFilePath(imagePath);
+        final inputImage = InputImage.fromFilePath(imagePath); // 입력 이미지 생성
 
         final textRecognizer =
-            TextRecognizer(script: TextRecognitionScript.latin);
-        final recognizedText = await textRecognizer.processImage(inputImage);
-        String? formattedDate = _formatDate(recognizedText.text);
+            TextRecognizer(script: TextRecognitionScript.latin); // 텍스트 인식기 생성
+        final recognizedText =
+            await textRecognizer.processImage(inputImage); // 이미지에서 텍스트 인식
+        String? formattedDate =
+            _formatDate(recognizedText.text); // 인식된 텍스트에서 날짜 추출
 
-        callback(imageData, formattedDate);
+        callback(imageData, formattedDate); // 콜백 호출
 
-        textRecognizer.close();
+        textRecognizer.close(); // 텍스트 인식기 닫기
       } catch (e) {
-        print('Error capturing image: $e');
-        callback(null, null);
+        print('Error capturing image: $e'); // 오류 출력
+        callback(null, null); // 콜백 호출
       }
     }
   }
 
+  // 인식된 텍스트에서 날짜를 추출하는 메서드
   String? _formatDate(String text) {
-    RegExp dateRegex = RegExp(r'\b(\d{2}|\d{4})[.](\d{1,2})[.](\d{1,2})\b');
-    Iterable<RegExpMatch> matches = dateRegex.allMatches(text);
+    RegExp dateRegex =
+        RegExp(r'\b(\d{2}|\d{4})[.](\d{1,2})[.](\d{1,2})\b'); // 날짜 정규식 패턴
+    Iterable<RegExpMatch> matches = dateRegex.allMatches(text); // 정규식 매칭
 
     if (matches.isNotEmpty) {
       RegExpMatch? selectedMatch;
@@ -64,7 +72,7 @@ class CameraService {
       for (var match in matches) {
         String year = match.group(1)!;
         if (year.length == 2) {
-          year = '20$year';
+          year = '20$year'; // 연도를 4자리로 변환
         }
 
         if (selectedMatch == null) {
@@ -72,7 +80,7 @@ class CameraService {
         } else {
           String selectedYear = selectedMatch.group(1)!;
           if (selectedYear.length == 2) {
-            selectedYear = '20$selectedYear';
+            selectedYear = '20$selectedYear'; // 연도를 4자리로 변환
           }
 
           if (int.parse(year) > int.parse(selectedYear)) {
@@ -101,77 +109,80 @@ class CameraService {
         String day = selectedMatch.group(3)!;
 
         if (year.length == 2) {
-          year = '20$year';
+          year = '20$year'; // 연도를 4자리로 변환
         }
 
-        return '$year-$month-$day';
+        return '$year-$month-$day'; // 날짜 형식으로 반환
       }
     }
-    return null;
+    return null; // 날짜가 없는 경우 null 반환
   }
 
+  // 바코드로 제품 정보를 가져오는 메서드
   Future<void> getProduct(
       String barcode,
       TextEditingController productNameController,
       Function(String) updateImageUrl) async {
-    var apiKey = '08250c6f4a19422781f0';
+    var apiKey = '08250c6f4a19422781f0'; // API 키
     var url = Uri.parse(
-        'http://openapi.foodsafetykorea.go.kr/api/$apiKey/I2570/json/1/5/BRCD_NO=$barcode');
-    var response = await http.get(url);
+        'http://openapi.foodsafetykorea.go.kr/api/$apiKey/I2570/json/1/5/BRCD_NO=$barcode'); // API URL
+    var response = await http.get(url); // HTTP GET 요청
 
     if (response.statusCode == 200) {
-      var jsonResponse = jsonDecode(response.body);
-      var productName = jsonResponse['I2570']['row'][0]['PRDT_NM'];
-      productNameController.text = productName;
-      await fetchGoogleImages(productName, updateImageUrl);
+      var jsonResponse = jsonDecode(response.body); // JSON 응답 파싱
+      var productName = jsonResponse['I2570']['row'][0]['PRDT_NM']; // 제품명 추출
+      productNameController.text = productName; // 제품명 입력 필드에 설정
+      await fetchGoogleImages(productName, updateImageUrl); // 구글 이미지 검색
     } else {
-      print('Request failed with status: ${response.statusCode}.');
+      print('Request failed with status: ${response.statusCode}.'); // 오류 출력
     }
   }
 
+  // 구글 이미지 검색을 통해 이미지 URL을 가져오는 메서드
   Future<void> fetchGoogleImages(
       String keyword, Function(String) updateImageUrl) async {
-    final response = await http
-        .get(Uri.parse('https://www.google.com/search?q=$keyword&tbm=isch'));
+    final response = await http.get(Uri.parse(
+        'https://www.google.com/search?q=$keyword&tbm=isch')); // 구글 이미지 검색 요청
     if (response.statusCode == 200) {
-      final document = parser.parse(response.body);
-      final elements = document.getElementsByTagName('img');
+      final document = parser.parse(response.body); // HTML 응답 파싱
+      final elements = document.getElementsByTagName('img'); // img 태그 요소 추출
       final urls = elements
-          .map((element) => element.attributes['src'])
-          .where((src) => src != null && src!.startsWith('http'))
+          .map((element) => element.attributes['src']) // src 속성 추출
+          .where((src) => src != null && src!.startsWith('http')) // 유효한 URL 필터링
           .toList();
       if (urls.isNotEmpty) {
-        imageUrl = urls.first!;
-        updateImageUrl(imageUrl);
+        imageUrl = urls.first!; // 첫 번째 이미지 URL 설정
+        updateImageUrl(imageUrl); // 콜백을 통해 이미지 URL 업데이트
       }
     } else {
-      throw Exception('Failed to load images');
+      throw Exception('Failed to load images'); // 오류 발생 시 예외 던지기
     }
   }
 
+  // 카메라 미리보기를 시작하는 메서드
   Future<void> startCameraPreview(
       BuildContext context, Function(Uint8List?, String?) callback) async {
-    await handlePermission(Permission.camera);
-    await handlePermission(Permission.microphone);
+    await handlePermission(Permission.camera); // 카메라 권한 요청
+    await handlePermission(Permission.microphone); // 마이크 권한 요청
     if (_cameraController != null && _cameraController!.value.isInitialized) {
       await Navigator.push(
         context,
         MaterialPageRoute(
           builder: (context) => Scaffold(
-            appBar: AppBar(title: const Text('Camera')),
+            appBar: AppBar(title: const Text('Camera')), // 앱 바 설정
             body: Stack(
               children: [
-                CameraPreview(_cameraController!),
+                CameraPreview(_cameraController!), // 카메라 미리보기
                 Align(
                   alignment: Alignment.bottomCenter,
                   child: Padding(
                     padding: const EdgeInsets.all(16.0),
                     child: FloatingActionButton(
                       onPressed: () async {
-                        await takePicture(callback);
+                        await takePicture(callback); // 사진 촬영
                         Navigator.pop(context); // 촬영 후 뒤로 돌아가기
                       },
-                      child: const Icon(Icons.camera),
+                      child: const Icon(Icons.camera), // 카메라 아이콘
                     ),
                   ),
                 ),
@@ -183,24 +194,25 @@ class CameraService {
     }
   }
 
+  // 원형 버튼을 생성하는 메서드
   Widget buildCircle(Alignment alignment, double size, Color color,
       Function onPressed, String? imageUrl) {
     return Align(
       alignment: alignment,
       child: InkWell(
         onTap: () {
-          onPressed();
+          onPressed(); // 버튼 클릭 시 호출할 콜백 함수
         },
         child: Container(
-          width: size,
-          height: size,
+          width: size, // 원의 너비 설정
+          height: size, // 원의 높이 설정
           decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: color,
+            shape: BoxShape.circle, // 원형 모양 설정
+            color: color, // 배경 색상 설정
             image: imageUrl != null && imageUrl.isNotEmpty
                 ? DecorationImage(
-                    image: NetworkImage(imageUrl),
-                    fit: BoxFit.cover,
+                    image: NetworkImage(imageUrl), // 이미지 설정
+                    fit: BoxFit.cover, // 이미지 맞춤 설정
                   )
                 : null,
           ),
@@ -209,7 +221,8 @@ class CameraService {
     );
   }
 
+  // 카메라 컨트롤러 리소스를 해제하는 메서드
   void dispose() {
-    _cameraController?.dispose();
+    _cameraController?.dispose(); // 카메라 컨트롤러 해제
   }
 }
